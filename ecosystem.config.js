c34710e43ee250fa3f50e35c08a61cc12404a7ba
wanjiasu <11630918+wanjiasu@user.noreply.gitee.com>
2026-01-14 11:30:45 +0800

    添加pm2文件

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'tradingagents-api',
       script: '.venv/bin/python',
-      args: '-m uvicorn api.server:app --host 0.0.0.0 --port 8000'
+      args: '-m uvicorn api.server:app --host 0.0.0.0 --port 8004'
     }
   ]
 };
